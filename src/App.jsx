fd1b34418baeb878a0e89cc16215009746ae515b
Saifshr <ssaifssyed17@gmail.com>
2025-10-30 13:00:20 +0530

    Added Search Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
+
 
 function App() {
+  const [searchTerm, setSearchTerm]=useState('')
   return (
     <div>
-      
+      <main>
+        <div className='pattern'/>
+        <div className='wrapper'>
+          <header>
+            <img src="./hero-img.png" alt="hero" />
+            <h1>Find Your favourite <span className='text-gradient'>Movies</span> Here!!</h1>
+          </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        </div>
+      </main>
+
     </div>
   )
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Search({searchTerm, setSearchTerm}) {
+  return (
+    <div className='search'>
+        <div>
+            <img src="search.svg" alt="search" />
+            <input type="text" placeholder='Search Movies You Like..' value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}/>
+        </div>
+      
+
+    </div>
+  )
+}
+
+export default Search
